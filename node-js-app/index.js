6ba7da9c0ec7547d3a4a9a0eacd42e450862df09
goktemkirez <ogkirez@gmail.com>
2022-08-18 18:54:23 +0300

    some tests

---
+++ node-js-app/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const { readFile } = require("fs");
+
+app.get('/', (request, response) => {
+    readFile('./home.html', 'utf8', (err, html) => {
+        if(err){
+            response.status(500).send('sorry, out of order')
+        }
+
+        response.send(html);
+    })
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
