ba214aba178e3ffb3887667a286effe37adf0a15
goktemkirez <ogkirez@gmail.com>
2022-08-22 14:37:05 +0300

    Color Transition

--- node-js-app/index.js
+++ node-js-app/index.js
@@ -5,17 +5,19 @@ const anagramRoutes = require("./pages/Anagram/Anagram");
 const rotationRoutes = require("./pages/Rotation/Rotation");
 const reverseRoutes = require("./pages/Reverse/Reverse");
 const frequencyRoutes = require("./pages/Frequency/Frequency");
+const randomStringRoutes = require("./pages/RandomString/RandomString");
 const visualAlignmentRoutes = require("./pages/VisualAlignment/VisualAlignment");
 const titleCaseRoutes = require("./pages/TitleCase/TitleCase");
-const randomStringRoutes = require("./pages/RandomString/RandomString");
+const colorTransitionRoutes = require("./pages/ColorTransition/ColorTransition");
 app.use("/palindrome/", palindromeRoutes);
 app.use("/anagram/", anagramRoutes);
 app.use("/rotation/", rotationRoutes);
 app.use("/reverse/", reverseRoutes);
 app.use("/frequency/", frequencyRoutes);
+app.use("/random-string/", randomStringRoutes);
 app.use("/visual-alignment/", visualAlignmentRoutes);
 app.use("/title-case/", titleCaseRoutes);
-app.use("/random-string/", randomStringRoutes);
+app.use("/color-transition/", colorTransitionRoutes);
  
 app.get('/', (request, response) => {
     response.sendFile(__dirname + "/index.html");
